3b8180aa0504cbdc8ceab8bd924f7ef6940eb3fc
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-12 11:23:19 +0545

    Add files via upload

---
+++ stackexample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    class stackexample
+    {
+        public static void Main(string []args)
+        {
+            Stack st = new Stack();
+            //push to the stack
+            st.Push("Central London");
+            st.Push("East London");
+            st.Push("North London");
+            st.Push("South East");
+            st.Push("Wembley");
+
+            // Checking whether the element "Wembley" is present in the stack or not
+            // Returns true if the element is present otherwise false
+            Console.WriteLine(st.Contains("Wembley"));
+            Console.ReadLine();
+            //Output is True
+        }
+    }
+}
